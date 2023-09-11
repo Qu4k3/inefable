481d3f0fe77eb9c30490bdbb8d036b8aa456c5af
Qu4k3 <paul.guillamon@gmail.com>
2023-09-11 11:10:33 +0200

    Fixes manifes + <Head> warning

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import countdown from '../scripts/countdown'
 import { useEffect } from 'react';
 
@@ -9,6 +10,18 @@ export default function Home() {
 
   return (
     <>
+      <Head>  
+        <title>Inefable Day · Boda de Isa & Paul · 02.11.24</title> 
+        <meta name="description" content="¡Te damos la bienvenida al rincón especial de nuestra boda! Aquí encontrarás información y hermosos recuerdos de nuestro gran día." />
+        <meta property="og:title" content="Inefable Day · Boda de Isa & Paul · 02.11.24" />
+        <meta property="og:description" content="¡Te damos la bienvenida al rincón especial de nuestra boda! Aquí encontrarás información y hermosos recuerdos de nuestro gran día." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://inefable.day" />
+        <meta property="og:image" content="https://inefable.day/thumbnail.png" />
+        <meta property="og:image:alt" content="inefable" />
+        <meta property="og:image:width" content="256" />
+        <meta property="og:image:height" content="256" />
+      </Head>
       <main>
         <div class="wrapper">
           <h1>inefable</h1>
