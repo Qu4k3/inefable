7580e6828232d73a7ab842cd3cd757c90c7e1a11
Qu4k3 <paul.guillamon@gmail.com>
2023-09-11 11:31:28 +0200

    ADD more icon sizes meta icon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,8 +7,10 @@ export default function Document() {
       <Head>        
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="manifest" href="/manifest.webmanifest" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/app.webmanifest" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Dawning+of+a+New+Day&family=Mulish:wght@200;300&display=swap" rel="stylesheet" />
