83f1ed4026f9d56d7917e705bf2c2b3bb5cf3c71
Qu4k3 <paul.guillamon@gmail.com>
2024-06-01 22:39:39 +0200

    FIX font definition

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,12 @@ import { Mulish } from "next/font/google";
 import { openGraphImage } from './shared-metadata'
 import '@/styles/globals.css'
 
-const mulish = Mulish({ subsets: ["latin"] });
+const mulish = Mulish({ 
+  weight: ['200', '300'],
+  style: 'normal',
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export const metadata = {
   title: "Inefable Day · Boda de Isa & Paul · 02.11.24",
