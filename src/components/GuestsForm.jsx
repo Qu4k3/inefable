29320a65d4033cf62180c5c36692fb042962913b
Qu4k3 <paul.guillamon@gmail.com>
2024-07-05 08:56:40 +0200

    ADD wiggle button effect + FIX button select tag

--- src/components/GuestsForm.jsx
+++ src/components/GuestsForm.jsx
@@ -3,60 +3,109 @@ import { useRouter } from 'next/navigation';
 import { updateGuestsForm } from "@/app/actions"
 import { useForm } from "react-hook-form"
 import { useState } from 'react';
+import { Button } from '@mantine/core';
 
-export function GuestForm({ rows }) {
+export function GuestForm({ rows, formSubmitted }) {
   const router = useRouter();
 
-  const [edited, setEdited] = useState([])
+  const [localState, setLocalState] = useState(rows);
+  const [wiggle, setWiggle] = useState(false);
 
   const {
     register,
     handleSubmit,
-    // formState: { errors },
+    watch
   } = useForm()
 
-  const handleChange = (id) => () => {
-    if (!edited.includes(id)) {
-      setEdited([...edited, id]);
+  const handleChange = (id) => {
+    const updatedRows = localState.map((row) => {
+      if (row.id === id) {
+        return { ...row, confirmed_assistance: !row.confirmed_assistance };
+      }
+      return row;
+    });
+
+    setLocalState(updatedRows)
+
+    setTimeout(() => {
+      setWiggle(true);
+      // setTimeout(() => setWiggle(false), 3000);
+    }, 2000);
+  }
+
+  /*useEffect(() => {
+    if (JSON.stringify(rows) !== JSON.stringify(localState)) {
+      setIsSubmitDisabled(false);
     }
 
-    console.log('edited', edited)
-  }
+    if (JSON.stringify(rows) === JSON.stringify(localState)) {
+      setIsSubmitDisabled(true);
+    }
+  }, [localState])*/
 
   const onSubmit = async (data) => {
     updateGuestsForm(data)
+    setWiggle(false)
     router.refresh()
+    //router.push('?confirmed=true', { scroll: false });
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      {rows
-        .sort((a, b) => a.id - b.id)
-        .map((row) => (
-          <label
-            key={row.id}
-            onClick={handleChange(row.id)}
-            className={edited.includes(row.id) ? 'edited' : ''}
-          >
-            <div>
-              <input
-                type="checkbox"
-                name="guest"
-                {...register(`${row.id}`)}
-                defaultChecked={row.confirmed_assistance}
-              />
-              <span>{row.first_name} {row.last_name}</span>
-            </div>
-            {row.confirmed_assistance !== null &&
-              <small
-                className={!row.confirmed_assistance ? 'no-assistant' : undefined}
-              >{row.confirmed_assistance ? '¡Asistiré!' : 'No asistiré'}</small>
-            }
-          </label>
-        ))}
-      {/*errors.exampleRequired && <span>This field is required</span>*/}
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {rows
+          .sort((a, b) => a.id - b.id)
+          .map((row) => {
+            const isChecked = watch(`${row.id}`, row.confirmed_assistance);
 
-      <button disabled={edited.length === 0}>Confirmar</button>
-    </form>
+            return (
+              <label
+                key={row.id}
+                onClick={() => handleChange(row.id)}
+              >
+                <div>
+                  <input
+                    type="checkbox"
+                    name="guest"
+                    {...register(`${row.id}`)}
+                    defaultChecked={row.confirmed_assistance}
+                  />
+                  <span>{row.first_name} {row.last_name}</span>
+                </div>
+                {(row.confirmed_assistance !== null || isChecked) &&
+                  <small
+                    className={
+                      (!isChecked)
+                        ? 'no-assistant'
+                        : 'assistant'
+                    }
+                  >
+                    {
+                      !isChecked
+                        ? 'No asistiré'
+                        : isChecked && !row.confirmed_assistance
+                          ? '¡Asistiré!'
+                          : row.confirmed_assistance
+                            ? '¡Confirmado!'
+                            : ''
+                    }
+                  </small>
+                }
+              </label>
+            )
+          })}
+        <Button
+          variant="default"
+          color="#e1a9bf"
+          radius="xl"
+          size="md"
+          type='submit'
+          classNames={{
+            root: wiggle ? 'wiggle' : ''
+          }}        >
+          Confirmar
+        </Button>
+      </form>
+    </>
   )
 }
