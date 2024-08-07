cfe5c192201b6c5455d26d5945173686809e7cfd
Paul Guillamon <Qu4k3@users.noreply.github.com>
2024-08-07 15:26:16 +0000

    TRY Header without SSR

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import FAQ from "@/components/sections/FAQ";
 import Footer from "@/components/sections/Footer";
 import CheckForm from "@/components/forms/CheckForm";
 import Hashtag from "@/components/sections/Hashtag";
-import Header from "@/components/sections/Header";
+//import Header from "@/components/sections/Header";
 import Inefable from "@/components/sections/Inefable";
 import Map from "@/components/sections/Map";
 import Playlist from "@/components/sections/Playlist";
@@ -14,21 +14,29 @@ import Menu from "@/components/elements/Menu";
 import Audio from "@/components/elements/Audio";
 import FlowerSeparator from "@/components/elements/FlowerSeparator";
 import { useEffect, useState } from "react";
+import dynamic from 'next/dynamic';
+
+// Import dynamically with no SSR
+const Header = dynamic(() => import('@/components/sections/Header'), { ssr: false });
 
 export default function Home() {
   const [pageVisited, setPageVisited] = useState(false);
 
   useEffect(() => {
-    const hasVisited = localStorage.getItem('hasVisited');
-    if (hasVisited) {
-      setPageVisited(true);
+    if (typeof window !== 'undefined') {
+      const hasVisited = localStorage.getItem('hasVisited');
+      if (hasVisited) {
+        setPageVisited(true);
+      }
     }
   }, []);
 
   const handleVisitClick = () => {
     setPageVisited(true);
-    localStorage.setItem('hasVisited', 'true');
-    localStorage.setItem('playSong', 'true');
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('hasVisited', 'true');
+      localStorage.setItem('playSong', 'true');
+    }
   };
 
   return (
