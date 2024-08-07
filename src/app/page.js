34d4c0d1bb5c2110941484407a9f4cce2d531004
Paul Guillamon <Qu4k3@users.noreply.github.com>
2024-08-07 15:50:36 +0000

    V3

--- src/app/page.js
+++ src/app/page.js
@@ -13,71 +13,32 @@ import Schedule from "@/components/sections/Timeline";
 import Menu from "@/components/elements/Menu";
 import Audio from "@/components/elements/Audio";
 import FlowerSeparator from "@/components/elements/FlowerSeparator";
-import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [pageVisited, setPageVisited] = useState(false);
-
-  // ADD an event listener to a button with id #btn-enter-website and triger if clicked
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const hasVisited = localStorage.getItem('hasVisited');
-      if (hasVisited) {
-        setPageVisited(true);
-      }
-      
-      const handleEnterWebsite = () => {
-        localStorage.setItem('hasVisited', 'true');
-        setPageVisited(true);
-      };
-
-      const button = document.getElementById('btn-enter-website');
-      if (button) {
-        button.addEventListener('click', handleEnterWebsite);
-      }
-
-      // Cleanup event listener on unmount
-      return () => {
-        if (button) {
-          button.removeEventListener('click', handleEnterWebsite);
-        }
-      };
-    }
-  }, []);
-
   return (
     <main className="home">
-      {!pageVisited ? (
-        <Header
-          hasVisited={pageVisited}
-        />
-      ) : (
-        <>
-          <Audio />
-          <Header hasVisited={pageVisited} />
-          <Schedule />
-          <FlowerSeparator />
-          <CheckForm />
-          <FlowerSeparator />
-          <Inefable />
-          <FlowerSeparator />
-          <DressCode />
-          <FlowerSeparator />
-          <Map />
-          <FlowerSeparator />
-          <FAQ />
-          <FlowerSeparator />
-          <Playlist />
-          <FlowerSeparator />
-          <Album />
-          <FlowerSeparator />
-          <Hashtag />
-          <FlowerSeparator />
-          <Footer />
-          <Menu />
-        </>
-      )}
-    </main>
+      <Audio />
+      <Header />
+      <Schedule />
+      <FlowerSeparator />
+      <CheckForm />
+      <FlowerSeparator />
+      <Inefable />
+      <FlowerSeparator />
+      <DressCode />
+      <FlowerSeparator />
+      <Map />
+      <FlowerSeparator />
+      <FAQ />
+      <FlowerSeparator />
+      <Playlist />
+      <FlowerSeparator />
+      <Album />
+      <FlowerSeparator />
+      <Hashtag />
+      <FlowerSeparator />
+      <Footer />
+      <Menu />
+    </main >
   );
 }
