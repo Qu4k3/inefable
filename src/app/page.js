7acb5a9884e1a5737f79a8123e16860161f583b8
Paul Guillamon <Qu4k3@users.noreply.github.com>
2024-08-07 15:56:52 +0000

    FIX typing mistake

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import Footer from "@/components/sections/Footer";
 import CheckForm from "@/components/forms/CheckForm";
 import Hashtag from "@/components/sections/Hashtag";
 import Header from "@/components/sections/Header";
-import Inefable from "@/cPomponents/sections/Inefable";
+import Inefable from "@/components/sections/Inefable";
 import Map from "@/components/sections/Map";
 import Playlist from "@/components/sections/Playlist";
 import Schedule from "@/components/sections/Timeline";
