dd338f12d4ba15669cf2971dd355b85cbb7c673d
Qu4k3 <paul.guillamon@gmail.com>
2024-10-22 22:48:18 +0200

    ADD QR redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/qr',
+        destination: 'https://photos.app.goo.gl/mQF6BhQh1ZoCmH6W7',
+        permanent: false,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
